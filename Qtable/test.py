104b0f60cee8fc9a804f8ff95ca875f0047293fc
BluePhragmites <34887191+BluePhragmites@users.noreply.github.com>
2020-10-20 23:59:41 +0800

    Update test.py
    random environment: fix start point and random end point

--- Qtable/test.py
+++ Qtable/test.py
@@ -1,26 +1,22 @@
-# hzy
-# hzy
-# 2020/10/18/19:12
-# test
-
 import numpy as np
 import pandas as pd
 import time
 import pickle
 
-np.random.seed(2)
+#np.random.seed(2)
 
 MAPHEIGHT = 5
 MAPWEIGHT = 5
 ACTIONS = ['left','right','up','down']
-ENV_STATE = {'start','empty','end'}
+ENV_STATE = {'start':'start','empty':'empty','end':'end'}
 ENV_REWARD = {'start':0,'empty':0,'end':1}
 
 EPSILON = 0.9   # greedy police
 ALPHA = 0.1     # learning rate
 GAMMA = 0.9    # discount factor
-MAX_EPISODES = 13   # maximum episodes
-FRESH_TIME = 0.3    # fresh time for one move
+MAX_EPISODES = 20   # maximum episodes
+FRESH_TIME = 0.1    # fresh time for one move
+ITERITION  = 100
 
 def getDataFrameLen(dataframe):
     #if dataframe.dtype == 'ndarry'
@@ -28,7 +24,6 @@ def getDataFrameLen(dataframe):
     colum = len(dataframe.values[0])
     row = np.size(dataframe.values)//colum
     return (row*(colum-1))
-
 
 def getIndex(state):
     return (MAPWEIGHT*state[0,1] + state[0,0])
@@ -51,111 +46,114 @@ def choose_action(state,q_table):
         action_name = state_actions.idxmax()
     return action_name
 
-def get_env_feedback(state,action,env,env_reward):
+def get_env_feedback(state,action,env,env_state,env_reward):
     #ACTIONS = ['left', 'right', 'up', 'down']
     nextState = state.copy()
     if action == 'left':
-        if state[0,0]== 0:
-            nextState[0,0] = state[0,0]
-        else:
-            nextState[0,0] = state[0,0] - 1
-
-        if env[nextState[0,0],nextState[0,0]] == 'start':
-            reward = env_reward['start']
-        elif env[nextState[0,0],nextState[0,0]] == 'empty':
-            reward = env_reward['empty']
-        else:
-            reward = env_reward['end']
-            nextState = 'terminal'
-
-    elif action == 'right':
-        if state[0,0]== MAPWEIGHT-1:
-            nextState[0,0] = state[0,0]
-        else:
-            nextState[0,0] = state[0,0] + 1
-
-        if env[nextState[0,0],nextState[0,1]] == 'start':
-            reward = env_reward['start']
-        elif env[nextState[0,0],nextState[0,1]] == 'empty':
-            reward = env_reward['empty']
-        else:
-            reward = env_reward['end']
-            nextState = 'terminal'
-
-    elif action == 'up':
-        if state[0,1] == 0:
+        if state[0,1]== 0:
             nextState[0,1] = state[0,1]
         else:
             nextState[0,1] = state[0,1] - 1
 
-        if env[nextState[0,0],nextState[0,1]] == 'start':
+        if env[nextState[0,0]][nextState[0,1]] == env_state['start']:
             reward = env_reward['start']
-        elif env[nextState[0,0],nextState[0,1]] == 'empty':
+        elif env[nextState[0,0]][nextState[0,1]] == env_state['empty']:
             reward = env_reward['empty']
-        else:
+        elif env[nextState[0,0]][nextState[0,1]] == env_state['end']:
             reward = env_reward['end']
             nextState = 'terminal'
+        else:
+            reward = env_reward['empty']
 
-    #elif action == 'down':
-    else :
-        if state[0,1] == MAPHEIGHT - 1:
+    elif action == 'right':
+        if state[0,1]== MAPWEIGHT-1:
             nextState[0,1] = state[0,1]
         else:
             nextState[0,1] = state[0,1] + 1
 
-        if env[nextState[0,0],nextState[0,1]] == 'start':
+        if env[nextState[0,0]][nextState[0,1]] == env_state['start']:
             reward = env_reward['start']
-        elif env[nextState[0,0],nextState[0,1]] == 'empty':
+        elif env[nextState[0,0]][nextState[0,1]] == env_state['empty']:
             reward = env_reward['empty']
-        else:
+        elif env[nextState[0,0]][nextState[0,1]] == env_state['end']:
             reward = env_reward['end']
             nextState = 'terminal'
+        else:
+            reward = env_reward['empty']
+
+    elif action == 'up':
+        if state[0,0] == 0:
+            nextState[0,0] = state[0,0]
+        else:
+            nextState[0,0] = state[0,0] - 1
+
+        if env[nextState[0,0]][nextState[0,1]] == env_state['start']:
+            reward = env_reward['start']
+        elif env[nextState[0,0]][nextState[0,1]] == env_state['empty']:
+            reward = env_reward['empty']
+        elif env[nextState[0,0]][nextState[0,1]] == env_state['end']:
+            reward = env_reward['end']
+            nextState = 'terminal'
+        else:
+            reward = env_reward['empty']
+
+    #elif action == 'down':
+    else :
+        if state[0,0] == MAPHEIGHT - 1:
+            nextState[0,0] = state[0,0]
+        else:
+            nextState[0,0] = state[0,0] + 1
+
+        if env[nextState[0,0]][nextState[0,1]] == env_state['start']:
+            reward = env_reward['start']
+        elif env[nextState[0,0]][nextState[0,1]] == env_state['empty']:
+            reward = env_reward['empty']
+        elif env[nextState[0,0]][nextState[0,1]] == env_state['end']:
+            reward = env_reward['end']
+            nextState = 'terminal'
+        else:
+            reward = env_reward['empty']
 
     return nextState,reward
 
-def update_env(state,env):
+def update_env(state,env,env_state):
     if state == 'terminal':
-        showEnv(state,env)
+        showEnv(state,env,env_state)
         time.sleep(2)
     else:
-        showEnv(state, env)
+        showEnv(state, env,env_state)
         time.sleep(FRESH_TIME)
 
-def showEnv(state,env):
+def showEnv(state,env,env_state):
     for iTemp in range(MAPWEIGHT):
         for jTemp in range(MAPHEIGHT):
             if state == 'terminal':
-                if env[iTemp][jTemp] == 'end':
-                    print('T')
+                if env[iTemp][jTemp] == env_state['end']:
+                    print('T',end='')
                 else:
                     print('*',end='')
             else:
-            #    print(state[0,0])
-            #    print(state[0,1])
                 if ((iTemp == state[0,0]) and (jTemp == state[0,1])):
                     print('o',end='')
-                elif env[iTemp][jTemp] == 'end':
+                elif env[iTemp][jTemp] == env_state['end']:
                     print('T', end='')
                 else:
                     print('*',end='')
-            '''
-            if env[iTemp,jTemp] == 0:
-                print('-')
-            '''
         print('')
+    print('')
 
-def rl(env,env_reward,pre_table):
+def rl(env,env_state,env_reward,pre_table):
     # main part of RL loop
     q_table = pre_table.copy()
     for episode in range(MAX_EPISODES):
-        state = findHead(env)
+        state = findHead(env,env_state)
         is_terminated = False
-        update_env(state, env)
+        # update_env(state,env,env_state)
         while not is_terminated:
             action = choose_action(state, q_table)
-            nextState, reward = get_env_feedback(state,action,env,env_reward)  # take action & get next state and reward
+            nextState, reward = get_env_feedback(state,action,env,env_state,env_reward)  # take action & get next state and reward
             q_predict = q_table.loc[getIndex(state), action]
-            if nextState!= 'terminal':
+            if nextState != 'terminal':
                 q_target = reward + GAMMA * q_table.iloc[getIndex(nextState), :].max()  # next state is not terminal
             else:
                 q_target = reward  # next state is terminal
@@ -164,11 +162,11 @@ def rl(env,env_reward,pre_table):
             q_table.loc[getIndex(state),action] += ALPHA * (q_target - q_predict)  # update
             state = nextState  # move to next state
 
-            update_env(state, env)
-            print(q_table)
+            #update_env(state, env, env_state)
+            #print(q_table)
     return q_table
 
-def build_env(weight,height):
+def build_env(weight,height,env_state):
     #env = np.zeros((MAPWEIGHT, MAPHEIGHT))
     env = []
     for iTemp in range(weight):
@@ -176,60 +174,57 @@ def build_env(weight,height):
         envTemp = []
         for jTemp in range(height):
             #env[iTemp,jTemp] = envState['empty']
-            envTemp.append('empty')
+            envTemp.append(env_state['empty'])
         env.append(envTemp)
 
 
-    startX = np.random.choice(range(weight))
-    startY = np.random.choice(range(height))
+    #startX = np.random.choice(range(weight))
+    #startY = np.random.choice(range(height))
+    startX = 0
+    startY = 0
     endX = np.random.choice(range(weight))
     endY = np.random.choice(range(height))
     while (startX, startY) == (endX, endY):
         endX = np.random.choice(range(weight))
         endY = np.random.choice(range(height))
-    env[startX][startY] = 'start'
-    env[endX][endY] = 'end'
+    env[startX][startY] = env_state['start']
+    env[endX][endY] = env_state['end']
     return env
 
-def findHead(env):
+def findHead(env,env_state):
     findflag = False
     for iTemp in range(MAPWEIGHT):
         for jTemp in range(MAPHEIGHT):
             #env[iTemp,jTemp] = envState['empty']
-            if env[iTemp][jTemp] == 'start':
+            if env[iTemp][jTemp] == env_state['start']:
                 findflag = True
                 break
         if findflag == True:
             break
-    return (iTemp*MAPWEIGHT+jTemp)
-
+    state = np.zeros((1,2),dtype='int')
+    state[0, 0] = iTemp
+    state[0, 1] = jTemp
+    return state
 
 if __name__ == "__main__":
+    for iTemp in range(ITERITION):
+        env = build_env(MAPWEIGHT, MAPHEIGHT, ENV_STATE)
+        try:
+            fp = open('Qtable.pickle','rb')
+        except Exception as e:
+            print(e)
+            pre_table = build_q_table(env,ACTIONS)
+        else:
+            fp.close()
+            pre_table = pd.read_pickle('Qtable.pickle')
+
+        q_table = rl(env,ENV_STATE,ENV_REWARD,pre_table)
+
+    timeData = time.localtime()
+    timeStr = "%04d%02d%02d%02d%02d_env.pickle"%(timeData.tm_year,timeData.tm_mday,
+                              timeData.tm_hour,timeData.tm_min,timeData.tm_sec)
     try:
-        fp = open('env.pickle','rb')
-    except Exception as e:
-        print(e)
-        env = build_env(MAPWEIGHT,MAPHEIGHT)
-    else:
-        fp.close()
-        env = pickle.load(fp)
-
-
-    try:
-        fp = open('Qtable.pickle','rb')
-    except Exception as e:
-        print(e)
-        pre_table = build_q_table(env,ACTIONS)
-    else:
-        fp.close()
-        pre_table = pd.read_pickle('Qtable.pickle')
-
-    q_table = rl(env,ENV_REWARD,pre_table)
-    print('\r\nQ-table:\n')
-    print(q_table)
-
-    try:
-        fp = open('env.pickle','wb')
+        fp = open(timeStr,'wb')
     except Exception as e:
         print(e)
         print("can't save the environment")
@@ -237,14 +232,9 @@ if __name__ == "__main__":
         pickle.dump(env, fp)
         fp.close()
 
-    q_table.to_pickle('Qtable.pickle')
-'''
-    try:
-        fp = open('Qtable.pickle','wb')
-    except Exception as e:
-        print(e)
-        print("can't save the Q table")
-    else:
-        q_table.to_pickle('Qtable.pickle')
-        fp.close()
-'''
+    timeStr = "%04d%02d%02d%02d%02d_Qtable.pickle" % (timeData.tm_year, timeData.tm_mday,
+                                              timeData.tm_hour, timeData.tm_min, timeData.tm_sec)
+    q_table.to_pickle(timeStr)
+    timeStr = "%04d%02d%02d%02d%02d_Qtable.csv" % (timeData.tm_year, timeData.tm_mday,
+                                                      timeData.tm_hour, timeData.tm_min, timeData.tm_sec)
+    q_table.to_csv(timeStr)
